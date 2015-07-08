5f1eb7f68e387d6046ce7d6ef253c8bf0db76601
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-08 00:09:19 -0400

    Update gh-pages

--- playground.source.js
+++ playground.source.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function () {
 	var input = document.getElementById('input');
 	var highlight = document.getElementById('highlight');
 	var output = document.getElementById('output');
+	var caret = document.getElementById('caret');
 
 	function update() {
 		var buffer = '';
@@ -12,6 +13,8 @@ document.addEventListener('DOMContentLoaded', function () {
 		var afterHTML = '';
 
 		try {
+			oncaretchange();
+
 			beforeHTML = Prism.highlight(input.value, Prism.languages.scss);
 
 			buffer = sassy.process(input.value, { safe: true });
@@ -44,11 +47,42 @@ document.addEventListener('DOMContentLoaded', function () {
 		highlight.scrollTop = input.scrollTop;
 	}
 
-	input.addEventListener('keydown', onkeydown);
+	function oncaretchange() {
+		var end = input.selectionEnd;
+
+		caret.innerHTML = input.value.substring(0, end).replace(/</g, '&lt;') + '<div id="cursor"></div>' + input.value.substring(end).replace(/</g, '&lt;');
+	}
+
+	input.addEventListener('keydown', function (event) {
+		onkeydown(event);
+
+		caret.setAttribute('down', '');
+
+		setTimeout(oncaretchange);
+
+		function onkeyup() {
+			caret.removeAttribute('down');
+
+			input.removeEventListener('keyup', onkeyup);
+		}
+
+		window.addEventListener('keyup', onkeyup);
+	});
 
 	input.addEventListener('input', update);
 
 	input.addEventListener('scroll', onscroll);
+
+	input.addEventListener('focus', oncaretchange);
+	input.addEventListener('mousedown', function () {
+		caret.setAttribute('down', '');
+		setTimeout(oncaretchange);
+
+		window.addEventListener('mouseup', function () {
+			caret.removeAttribute('down');
+			setTimeout(oncaretchange);
+		});
+	});
 
 	update();
 
