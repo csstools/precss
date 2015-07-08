42ece1d0bee51e3a810eed0ffb0c8ab21fac9c93
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-08 16:30:54 -0400

    Update gh-pages

--- playground.source.js
+++ playground.source.js
@@ -15,7 +15,7 @@ function toHash(string) {
 		.replace(/%40/g, '@');
 }
 
-var defaultString = location.href.slice(-1) === '#' || location.hash ? fromHash(location.hash.slice(1)) : '@define-mixin icon $name {\n\tpadding-left: 16px;\n\n\t&::after {\n\t\tcontent: "";\n\t\tbackground-url: url(/icons/$(name).png);\n\t}\n}\n\n$blue: color(#06f blackness(20%));\n$column: 200px;\n\n.search {\n\t@mixin icon search;\n}\n\n.menu {\n\tbackground: $blue;\n\twidth: calc(4 * $column);\n}\n\n.foo {\n\t@if 3 < 5 {\n\t\tbackground: green;\n\t}\n}\n\n@for $i from 10 to 30 by 10 {\n\t.b-$i { width: $(i)px; }\n}\n\n@each $icon in (foo, bar, baz) {\n\t.icon-$(icon) {\n\t\tbackground: url(icons/$icon.png);\n\t}\n}';
+var defaultString = location.href.slice(-1) === '#' || location.hash ? fromHash(location.hash.slice(1)) : '@define-mixin icon $name {\n\tpadding-left: 16px;\n\n\t&::after {\n\t\tcontent: "";\n\t\tbackground-url: url(/icons/$(name).png);\n\t}\n}\n\n$blue: #056ef0;\n$column: 200px;\n\n.search {\n\t@mixin icon search;\n}\n\n.menu {\n\tbackground: color($blue blackness(20%));\n\twidth: calc(4 * $column);\n}\n\n.foo {\n\t@if 3 < 5 {\n\t\tbackground: green;\n\t}\n}\n\n@for $i from 10 to 30 by 10 {\n\t.b-$i { width: $(i)px; }\n}\n\n@each $icon in (foo, bar, baz) {\n\t.icon-$(icon) {\n\t\tbackground: url(icons/$icon.png);\n\t}\n}';
 
 document.addEventListener('DOMContentLoaded', function () {
 	var input = document.getElementById('input');
