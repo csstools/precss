1c81f8b4fbcd63654906530b046d8742f7472759
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-08 23:32:30 -0400

    Update gh-pages

--- playground.source.js
+++ playground.source.js
@@ -183,12 +183,19 @@ function pen() {
 			.replace(/%40/g, '@');
 	}
 
+	function ontouchstart(event) {
+		event.preventDefault();
+
+		onpointerdown(event);
+	}
+
 	// bind events
 	input.addEventListener('focus', onfocus);
 	input.addEventListener('keydown', onkeydown);
 	input.addEventListener('input', oninput);
 	input.addEventListener('mousedown', onpointerdown);
 	input.addEventListener('scroll', onscroll);
+	input.addEventListener('touchstart', ontouchstart);
 
 	// set value
 	input.value = location.href.slice(-1) === '#' || location.hash ? fromHash(location.hash.slice(1)) : '@define-mixin icon $name {\n\tpadding-left: 16px;\n\n\t&::after {\n\t\tcontent: "";\n\t\tbackground-url: url(/icons/$(name).png);\n\t}\n}\n\n$blue: #056ef0;\n$column: 200px;\n\n.search {\n\t@mixin icon search;\n}\n\n.menu {\n\tbackground: color($blue blackness(20%));\n\twidth: calc(4 * $column);\n}\n\n.foo {\n\t@if 3 < 5 {\n\t\tbackground: green;\n\t} @else {\n\t\tbackground: yellow;\n\t}\n}\n\n@for $i from 10 to 30 by 10 {\n\t.b-$i { width: $(i)px; }\n}\n\n@each $icon in (foo, bar, baz) {\n\t.icon-$(icon) {\n\t\tbackground: url(icons/$icon.png);\n\t}\n}';
