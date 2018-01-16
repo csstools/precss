b6e214fdfae164cf581ffc2788fe81618372b747
Jonathan Neal <jonathantneal@hotmail.com>
2018-01-16 17:28:20 -0500

    3.0.0

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,4 +1,7 @@
-You want to help? You rock! Now, take a moment to be sure your contributions make sense to everyone else.
+# Contributing to PreCSS
+
+You want to help? You rock! Now, take a moment to be sure your contributions
+make sense to everyone else.
 
 ## Reporting Issues
 
@@ -11,50 +14,52 @@ Remember, a bug is a _demonstrable problem_ caused by _our_ code.
 
 ## Submitting Pull Requests
 
-Pull requests are the greatest contributions, so be sure they are focused in scope, and do avoid unrelated commits.
+Pull requests are the greatest contributions, so be sure they are focused in
+scope and avoid unrelated commits.
 
-1. To begin, [fork this project], clone your fork, and add our upstream.
+1. To begin; [fork this project], clone your fork, and add our upstream.
    ```bash
    # Clone your fork of the repo into the current directory
-   git clone https://github.com/<your-username>/precss
+   git clone git@github.com:YOUR_USER/precss.git
+
    # Navigate to the newly cloned directory
    cd precss
+
    # Assign the original repo to a remote called "upstream"
-   git remote add upstream https://github.com/jonathantneal/precss
-   # Install the tools necessary for development
+   git remote add upstream git@github.com:jonathantneal/precss.git
+
+   # Install the tools necessary for testing
    npm install
    ```
 
 2. Create a branch for your feature or fix:
    ```bash
-   # Move into a new branch for a feature
+   # Move into a new branch for your feature
    git checkout -b feature/thing
    ```
    ```bash
-   # Move into a new branch for a fix
+   # Move into a new branch for your fix
    git checkout -b fix/something
    ```
 
-3. Be sure your code follows our practices.
+3. If your code follows our practices, then push your feature branch:
    ```bash
    # Test current code
-   npm run test
+   npm test
    ```
-
-4. Push your branch up to your fork:
    ```bash
-   # Push a feature branch
+   # Push the branch for your new feature
    git push origin feature/thing
    ```
    ```bash
-   # Push a fix branch
-   git push origin fix/something
+   # Or, push the branch for your update
+   git push origin update/something
    ```
 
-5. Now [open a pull request] with a clear title and description.
+That’s it! Now [open a pull request] with a clear title and description.
 
 [already been reported]: issues
 [fork this project]:     fork
-[live example]:          http://codepen.io/pen
+[live example]:          https://codepen.io/pen
 [open a pull request]:   https://help.github.com/articles/using-pull-requests/
 [reduced test case]:     https://css-tricks.com/reduced-test-cases/
--- index.js
+++ index.js
@@ -1,109 +1,37 @@
-var postcss = require('postcss');
+// tooling
+import postcss from 'postcss';
+import postcssAdvancedVariables from 'postcss-advanced-variables';
+import postcssAtroot from 'postcss-atroot';
+import postcssExtendRule from 'postcss-extend-rule';
+import postcssNested from 'postcss-nested';
+import postcssPresetEnv from 'postcss-preset-env';
+import postcssPropertyLookup from 'postcss-property-lookup';
 
-// list of all plugins
-var processors = [
-	{
-		plugin:    require('postcss-partial-import'),
-		namespace: 'import',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-mixins'),
-		namespace: 'mixins',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-advanced-variables'),
-		namespace: 'variables',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-custom-media'),
-		namespace: 'media',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-custom-properties'),
-		namespace: 'properties',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-media-minmax'),
-		namespace: 'minmax',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-color-function'),
-		namespace: 'color',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-nesting'),
-		namespace: 'nesting',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-nested'),
-		namespace: 'nested',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-custom-selectors'),
-		namespace: 'selectors',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-atroot'),
-		namespace: 'atroot',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-property-lookup'),
-		namespace: 'lookup',
-		defaults:  {
-			logLevel: 'warn'
-		}
-	},
-	{
-		plugin:    require('postcss-extend'),
-		namespace: 'extend',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-selector-matches'),
-		namespace: 'matches',
-		defaults:  {}
-	},
-	{
-		plugin:    require('postcss-selector-not'),
-		namespace: 'not',
-		defaults:  {}
-	}
+// plugin chain
+const plugins = [
+	postcssExtendRule,
+	postcssAdvancedVariables,
+	postcssPresetEnv,
+	postcssAtroot,
+	postcssPropertyLookup,
+	postcssNested
 ];
 
-// load all plugins
-module.exports = postcss.plugin('precss', function (rawoptions) {
-	var options = rawoptions || {};
+// plugin
+export default postcss.plugin('precss', rawopts => {
+	// initialize options, defaulting preset-env to stage 0 features
+	const opts = Object.assign({ stage: 0 }, rawopts);
 
-	var instance = postcss();
+	// initialize all plugins
+	const initializedPlugins = plugins.map(
+		plugin => plugin(opts)
+	);
 
-	// for each plugin
-	processors.forEach(function (processor) {
-		var namespaceOptions = processor.namespace in options ? options[processor.namespace] : options;
-		var processorOptions = {};
-
-		Object.keys(processor.defaults).forEach(function (key) {
-			processorOptions[key] = processor.defaults[key];
-		});
-
-		Object.keys(namespaceOptions).forEach(function (key) {
-			processorOptions[key] = namespaceOptions[key];
-		});
-
-		if (namespaceOptions && !processorOptions.disable) {
-			instance.use(processor.plugin(processorOptions));
-		}
-	});
-
-	return instance;
+	// process css with all plugins
+	return (root, result) => initializedPlugins.reduce(
+		(promise, plugin) => promise.then(
+			() => plugin(result.root, result)
+		),
+		Promise.resolve()
+	);
 });
