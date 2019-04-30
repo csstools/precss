14a17d30bc4fe1e5db5d7feb99cd4096bde1a2fd
Maximilian Becher <maximilian.becher@ueberbit.de>
2019-04-30 17:42:15 +0200

    #127 add tests for passing down options to plugins add default options test, add global option test (options passed down to all plugins), add plugin-specific test (options passed to a specific plugin)

--- .tape.js
+++ .tape.js
@@ -24,6 +24,29 @@ module.exports = {
 			options: {
 				stage: 4
 			}
+		},
+		'options': {
+			message: 'supports passing no { options }'
+		},
+		'options:global': {
+			message: 'supports global { stage } usage ',
+			options: {
+				stage: false
 		}
+		},
+		'options:plugin-specific': {
+			message: 'supports passing grid and color-mod { options } to postcssPresetEnv only',
+			options: {
+				'postcssPresetEnv': {
+					features: {
+						'color-mod-function': false,
+					},
+					'autoprefixer': {
+						'grid': 'autoplace'
+					}
+
+				}
+			}
+		},
 	}
 };
